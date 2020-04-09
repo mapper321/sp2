138c184a03b6896a464dc38e0fa02ea5817fe0e6
mapper <547008022@qq.com>
2020-04-09 16:30:32 +0800

    修改服务名称，添加springcloud支持

--- src/main/java/com/hebei/Application.java
+++ src/main/java/com/hebei/Application.java
@@ -2,12 +2,16 @@ package com.hebei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author: mapper
  * @since: 2020/3/31
  */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/hebei/core/service/impl/MongoUploader.java
+++ src/main/java/com/hebei/core/service/impl/MongoUploader.java
@@ -2,7 +2,6 @@ package com.hebei.core.service.impl;
 
 import com.hebei.core.service.AbstractUploader;
 import com.hebei.core.service.template.MyGridFsTemplate;
-import com.hebei.core.util.StringUtil;
 import com.hebei.core.web.servlet.GridFsServlet;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import java.io.InputStream;
 
 /**
  * mongodb文件上传
+ *
  * @author: mapper
  * @since: 2020/4/2
  */
@@ -21,11 +21,11 @@ public class MongoUploader extends AbstractUploader {
     @Autowired
     MyGridFsTemplate myGridFsTemplate;
 
-    public String uploadImpl(InputStream in, String source, String fileName){
+    public String uploadImpl(InputStream in, String source, String fileName) {
         String path = null;
         String fileType = fileName.substring(fileName.lastIndexOf(".") + 1);
-        ObjectId objectId = myGridFsTemplate.store(in, fileName,fileType, source);
-        path = GridFsServlet.URI_PREFIX+source+"/"+objectId+"."+fileType;
+        ObjectId objectId = myGridFsTemplate.store(in, fileName, fileType, source);
+        path = GridFsServlet.URI_PREFIX + source + "/" + objectId + "." + fileType;
         return path;
     }
 }
